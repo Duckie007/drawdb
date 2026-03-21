5af314d8623eb32d85502ec2514552400962429e
Duckie007 <Duckie007@users.noreply.github.com>
2026-03-21 20:57:11 +0800

    feat: default to simplified Chinese

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,6 +1,5 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
 import { en, english } from "./locales/en";
 import { zh, chinese } from "./locales/zh";
 import { es, spanish } from "./locales/es";
@@ -109,10 +108,10 @@ export const languages = [
 ].sort((a, b) => a.name.localeCompare(b.name));
 
 i18n
-  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: "en",
+    lng: "zh",
+    fallbackLng: "zh",
     debug: false,
     interpolation: {
       escapeValue: false,
